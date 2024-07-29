e839d74bda440491282f127c46a53043a1d820f6
voltasteve <27285224-voltasteve@users.noreply.replit.com>
2024-07-29 20:58:16 +0000

    task 1

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# configuration with puppet
+
+exec { 'http header':
+        command => 'sudo apt-get update -y;
+        sudo apt-get install nginx -y;
+        sudo sed -i "/sever_name _/a add_header X-Served_By $HOSTNAME;" /etc/nginx/sites-available/default
+        sudo service nginx restart',
+        provide => shell,
+}
