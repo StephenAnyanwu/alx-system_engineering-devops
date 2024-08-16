f52ad112d2bf71a7fe387f8f2d10900fa9a02649
voltasteve <27285224-voltasteve@users.noreply.replit.com>
2024-08-16 03:16:40 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,22 +9,22 @@ import requests
 import sys
 
 # Note: 'requests' is a third-party library hence its installed
-url = "https://jsonplaceholder.typicode.com"
-arg = re.fullmatch(r'\d+', sys.argv[1])
-if arg:
-    try:
-        emplId = int(sys.argv[1])
-        tasks = requests.get(f"{url}/todos?userId={emplId}").json()
-        numTasks = len(tasks)
-        print("ok")
-        numCompTasks = len([x for x in tasks if x['completed']])
-        userData = requests.get(f"{url}/users/{emplId}")
-        userName = userData.json().get('name')
-        compTasksTitle = [x.get('title') for x in tasks if x.get('completed')]
-        print(f"Employee {userName} is done with\
-        tasks({numCompTasks}/{numTasks}):")
-        if numCompTasks > 0:
-            for title in compTasksTitle:
-                print(f"\t {title}")
-    except Exception as e:
-        pass
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    arg = re.fullmatch(r'\d+', sys.argv[1])
+    if arg:
+        try:
+            emplId = int(sys.argv[1])
+            tasks = requests.get(f"{url}/todos?userId={emplId}").json()
+            numTasks = len(tasks)
+            numCompTasks = len([x for x in tasks if x['completed']])
+            userData = requests.get(f"{url}/users/{emplId}")
+            userName = userData.json().get('name')
+            TasksTitle = [x.get('title') for x in tasks if x.get('completed')]
+            print(f"Employee {userName} is done with\
+            tasks({numCompTasks}/{numTasks}):")
+            if numCompTasks > 0:
+                for title in TasksTitle:
+                    print(f"\t {title}")
+        except Exception as e:
+            pass
