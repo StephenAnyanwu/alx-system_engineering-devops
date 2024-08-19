08cbce9ed106c988d42dd249500ec165595749ee
Stephen Anyanwu <voltasteve@gmail.com>
2024-08-19 16:03:24 -0700

    task 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,12 +20,12 @@ def number_of_subscribers(subreddit):
         The number of subscribers (not active users, total subscribers).
         0 if an invalid subreddit is given.
     """
-    url = "https://www.reddit.com/r/{subreddit}/about.json"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        num_subscribers = data['data']['subscribers']
+        num_subscribers = data.get('data').get('subscribers')
         return num_subscribers
     else:
         return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+     A script that queries the Reddit API for a given subreddit.
+'''
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    '''Queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+    If subreddit is not valid, it prints None.
+    '''
+    user = {'User-Agent': 'Mozilla/5.0'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
+    response = requests.get(url, headers=user, allow_redirects=False)
+    try:
+        for post in response.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
