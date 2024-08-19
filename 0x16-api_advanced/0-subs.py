83bde63c8ee3964c9bdb94b4f063d6afeb5f24f2
Stephen Anyanwu <voltasteve@gmail.com>
2024-08-19 15:19:07 -0700

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A python script that queries subscribers on a given Reddit API subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API for a given subreddit
+
+    Parameters
+    ----------
+    subredddit : str
+        Reddit API subreddit
+
+    Returns
+    -------
+    int
+        The number of subscribers (not active users, total subscribers).
+        0 if an invalid subreddit is given.
+    """
+    url = "https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        num_subscribers = data['data']['subscribers']
+        return num_subscribers
+    else:
+        return 0
